a75bb61d18e9fa608ade9ea0b5696c2517529e45
Toby Alden & stephen <student@epicodus.com>
2015-05-04 14:00:25 -0700

    Add Contact class and accompanying specs. Pass specs

--- lib/contact.rb
+++ lib/contact.rb
@@ -8,4 +8,17 @@ class Contact
     @emails = []
     @addresses = []
   end
+
+  define_method(:add_phone) do |phone|
+    @phones.push(phone)
+  end
+
+  define_method(:add_email) do |email|
+    @emails.push(email)
+  end
+
+  define_method(:add_address) do |address|
+    @addresses.push(address)
+  end
+
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -41,4 +41,31 @@ describe(Contact) do
     end
   end
 
+  describe('#add_phone') do
+    it("adds a phone to a contact's list of phones") do
+      test_contact = Contact.new({:name => "Greg"})
+      test_phone = Phone.new({:number => 5031234567, :type => 'Home'})
+      test_contact.add_phone(test_phone)
+      expect(test_contact.phones).to(eq([test_phone]))
+    end
+  end
+
+  describe('#add_email') do
+    it('adds an email to a contacts list of emails') do
+      test_contact = Contact.new({:name => 'Greg'})
+      test_email = Email.new({:address => 'lonelyboy16@yahoo.com', :type => "Work"})
+      test_contact.add_email(test_email)
+      expect(test_contact.emails()).to(eq([test_email]))
+    end
+  end
+
+  describe('#add_address') do
+    it('adds an address to a contacts list of addresses') do
+      test_contact = Contact.new({:name => 'Greg'})
+      test_address = Address.new({:address => '11201 Terrace Circle', :type => 'Home'})
+      test_contact.add_address(test_address)
+      expect(test_contact.addresses()).to(eq([test_address]))
+    end
+  end
+
 end
